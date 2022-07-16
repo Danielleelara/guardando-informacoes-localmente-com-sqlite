26c39a7f9a65a4989f260139939aaba6428e66a8
danielleelara <dany.sa1@hotmail.com>
2022-07-16 20:09:50 -0300

    feat: initial commit

---
+++ App.js
@@ -0,0 +1,37 @@
+import React ,{useState} from 'react'
+import { SafeAreaView, StatusBar, StyleSheet, FlatList } from "react-native"
+import AsyncStorage from "@react-native-async-storage/async-storage"
+import NotaEditor from "./src/componentes/NotaEditor"
+import Nota from './src/componentes/Nota';
+
+export default function App() { 
+  const [notas, setNotas] =useState([]);
+
+  async function mostraNotas(){
+    const todasChaves = await AsyncStorage.getAllKeys();
+    const todasNotas = await AsyncStorage.multiGet(todasChaves);
+    await AsyncStorage.multiGet(todasChaves)
+    setNotas(todasNotas)
+    console.log(todasNotas)
+  }
+  return (
+    <SafeAreaView style={estilos.container}>
+      <FlatList
+      data={notas}
+      renderItem={(nota)=><Nota {...nota}/>}
+      keyExtractor={notas => notas[0]}
+    />
+      <NotaEditor mostraNotas={mostraNotas}/>
+      <StatusBar/>
+    </SafeAreaView>
+  )
+}
+
+const estilos = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: "stretch",
+		justifyContent: "flex-start",
+	},
+})
+
